4e92a410ba5451377db3f36fd9bd23ff58333a25
Apurv <apurv2003londhe@gmail.com>
2025-06-28 00:48:56 +0530

    Updates_27_06_2025

---
+++ JavaScript Projects/Lucky Number Card using JavaScript and HTML and CSS/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let userEnterNumber = document.getElementById("user_Input");
+    let submitButton = document.getElementById("submit");
+    let reset = document.getElementById("reset");
+    let notLuckyNumber = document.querySelector(".notLuckyNumber");
+    let luckyNumber = document.querySelector(".luckyNumber");
+
+
+    let winningNumber = 1404;
+
+    submitButton.addEventListener("click", function () {
+        let enterNumber = parseInt(userEnterNumber.value);
+
+        if (/^\d{4}$/.test(enterNumber)) {
+            if (enterNumber === winningNumber) {
+                luckyNumber.style.display = "block"
+            }
+            else {
+                notLuckyNumber.style.display = "block"
+            }
+        }
+        else {
+            alert("Please enter a valid 4-digit number.")
+        }
+    })
+
+    reset.addEventListener("click", function () {
+        userEnterNumber.value = "";
+        luckyNumber.style.display = "none";
+        notLuckyNumber.style.display = "none";
+    })
+})
--- JavaScript Projects/Lucky Number Card/script.js
+++
@@ -1,8 +0,0 @@
-let winningNumber = 15;
-let userEnterNumber = prompt("Guess The Game Number: ");
-
-while (userEnterNumber != winningNumber) {
-    userEnterNumber= prompt("Guess Again!");
-}
-
-alert("Congratulations, You Enter Right Number !")
